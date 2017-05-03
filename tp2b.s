12167ab1c179a028c27c52b7a4d2c1c47a05dc43
Eduardo <massa-messo@hotmail.com>
2017-05-03 15:12:10 +0200

    SW1 fonctionnel

--- tp2b.s
+++ tp2b.s
@@ -1,29 +1,28 @@
-TRISG=0xBF886180
-TRISGCLR=TRISG+4
-TRISGINV=TRISG+12
-LATG=0xBF8861A0
-LATGINV=LATG+12
-CLR=4
+TRISF=0xBF886140
+TRISFCLR=TRISF+4
+TRISFINV=TRISF+12
+LATF=0xBF886160
+LATFINV=LATF+12
 
 
 	.text
 	.globl main
 	.ent main
 main:
-	# configuration RG6 en sortie
-	li $a0, TRISGCLR
-	li $a1, 0x00000040
+	# configuration RF6 en sortie
+	li $a0, TRISFCLR
+	li $a1, 0x00000000
 	sw $a1, 0($a0)
 
-	# chargement valeur LATG dans $a2
-	li $a2, LATG
+	# chargement valeur LATF dans $a2
+	li $a2, LATF
 	lw $a0, 0($a2)
 
 main_loop:
 	
-	# allumage LD4 (LATG6=1)
-	li $a0, LATGINV	
-	li $a1, 0x00000040
+	# allumage LD5 (LATF6=1)
+	li $a0, LATFINV	
+	li $a1, 0x00000001
 	sw $a1, 0($a0)
 	
 	#Boucle d'attente
